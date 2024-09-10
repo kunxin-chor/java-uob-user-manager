9960c2529cac48f53fb16f9a1352548c92fd42ce
kunxin-chor <chorkunxin@yahoo.com>
2024-09-10 07:04:42 +0000

    Updated main

--- src/main/java/com/example/UserManager.java
+++ src/main/java/com/example/UserManager.java
@@ -1,10 +1,15 @@
 package com.example;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserManager {
     List<User> users = new ArrayList<User>();
@@ -24,6 +29,18 @@ public class UserManager {
             gson.toJson(users, writer);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void loadUsers() {
+        try (FileReader reader = new FileReader(filePath)) {
+            Gson gson = new Gson();
+            Type userListType = new TypeToken<List<User>>(){}.getType();
+            users = gson.fromJson(reader, userListType);
+           
+        } catch (IOException e) {
+            e.printStackTrace();
+        
         }
     }
 }
