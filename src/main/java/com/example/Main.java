25884b5bd2ed8d3ffaf83722579ac81f3168db4a
kunxin-chor <chorkunxin@yahoo.com>
2024-09-10 07:01:58 +0000

    updated main branch

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,16 +1,13 @@
 package com.example;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<User> users = Arrays.asList(
-            new User("Alice", "alice@example.com"),
-            new User("Bob", "bob@example.com")
-        );
+     
 
         UserManager userManager = new UserManager("users.json");
-        userManager.saveUsers(users);
+        userManager.addUser(new User("Jon Snow", "jonsnow@asd.com"));
+        userManager.addUser(new User("Tony Stark", "tony@asd.com"));
+        userManager.saveUsers();
     }
 }
--- src/main/java/com/example/UserManager.java
+++ src/main/java/com/example/UserManager.java
@@ -3,16 +3,22 @@ package com.example;
 import com.google.gson.Gson;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserManager {
+    List<User> users = new ArrayList<User>();
     private String filePath;
 
     public UserManager(String filePath) {
         this.filePath = filePath;
     }
 
-    public void saveUsers(List<User> users) {
+    void addUser(User u) {
+        users.add(u);
+    }
+
+    public void saveUsers() {
         try (FileWriter writer = new FileWriter(filePath)) {
             Gson gson = new Gson();
             gson.toJson(users, writer);
